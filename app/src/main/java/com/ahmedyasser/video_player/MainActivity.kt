d058e6449cf93dd3b8aa08ee93b072da0a0932fc
Ahmed Yasser <aydevinquiries@gmail.com>
2024-08-10 14:31:31 +0300

    delete some fragments that i dont use anymore

--- app/src/main/java/com/ahmedyasser/video_player/MainActivity.kt
+++ app/src/main/java/com/ahmedyasser/video_player/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.ahmedyasser.video_player
 
+import android.net.Uri
+import android.net.Uri.parse
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import android.widget.MediaController
+import android.widget.VideoView
 import com.ahmedyasser.video_player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,38 +20,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        //refer to the videoView in the xml file
+        val videoView = findViewById<VideoView>(binding.testVideoView.id)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        //creating a media controller
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
-    }
+        //specify the location of the media file
+        val uri:Uri = parse("android.resource://$packageName/raw/sneakers_shop")
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        //setting mediaController and URI
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
     }
 }
